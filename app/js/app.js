170aa3c3bd8f6df8b02669262e927a0fd5844721
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-11-26 16:57:47 -0500

    routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-var npRefer = angular.module('npRefer', []);
+var npRefer = angular.module('npRefer', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "templates/start.html",
+                controller: "MainController"
+            })
+            .otherwise({redirectTo: "/main"});
+    });
