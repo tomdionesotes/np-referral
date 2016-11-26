68a5bd23d67137b883dc7490cc18477bd779623b
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-11-26 16:58:10 -0500

    add categories and issues

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,5 +1,35 @@
 'use strict;';
 
 npRefer.controller('MainController', function($scope){
-    $scope.test = 'Get started';
+    $scope.begin = 'Get started';
+
+    $scope.categories = [
+        {
+            name: "Foreign Policy",
+            issues: ["Iran", "China", "Russia", "Peace"]
+        },
+        {
+            name: "Work and Family",
+            issues: ["Minimum Wage", "Childcare"]
+        },
+        {
+            name: "Women",
+            issues: ["Access to Family Planning", "Equal Pay for Equal Work", "Protections against sexual abuse"]
+        },
+        {
+            name: "Immigration",
+            issues: ["Protections for dreamers", "Protections for refugees", "Protections for mulsim immigrants"]},
+        {
+            name: "Human Rights",
+            issues: ["Equality for minorities", "Equality for LGBT americans "]},
+        {
+            name: "Environment",
+            issues: ["Climate Change"]},
+        {
+            name: "Healthcare",
+            issues: ["Protect the ACA"]},
+        {
+            name: "Housing",
+            issues: ["Protect affordable housing"]}
+        ];
 });
